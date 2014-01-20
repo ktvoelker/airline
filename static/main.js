4f783ee967783f65d720a396ec81b5858c197a1e
Karl Voelker <ktvoelker@gmail.com>
2014-01-19 22:03:37 -0800

    Wrote a JS module for server communication

--- static/main.js
+++ static/main.js
@@ -1,7 +1,10 @@
 
-require(['ws'], function(ws) {
+require(['server'], function(server) {
 
   console.log("MAIN");
+  var conn = new server.Connection(console.log.bind(console));
+  conn.send({a: 3, b: 4});
+  conn.send({c: 5, d: 6});
 
 });
 
