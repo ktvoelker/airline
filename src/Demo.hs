0d60743555b7aacdb467a3c7cfa8b13afadcf335
Karl Voelker <karl@karlv.net>
2016-05-28 16:34:52 -0700

    Add flight types

--- src/Demo.hs
+++ src/Demo.hs
@@ -2,6 +2,7 @@
 module Demo where
 
 import Control.Concurrent.STM
+import qualified Data.Map as M
 import qualified Data.Set as S
 import H.IO
 import H.Prelude
@@ -10,30 +11,49 @@ import qualified CrossMap as CM
 import Object
 import Types
 
+model737800 :: Model
+model737800 =
+  Model
+  { _mName  = "Boeing 737-800"
+  , _mSpeed = 511
+  , _mRange = 6340
+  , _mSeats = 220
+  , _mCost  = 10000
+  }
+
 demo :: IO Game
 demo = atomically $ do
+  ordMovements <- newTQueue
   oHare <- newObject $ AirportState
-    { _apCode     = "ORD"
-    , _apName     = "Chicago O'Hare International Airport"
-    , _apCapacity = 2400
-    , _apAircraft = S.empty
+    { _apCode          = "ORD"
+    , _apName          = "Chicago O'Hare International Airport"
+    , _apCapacity      = 2400
+    , _apAircraft      = S.empty
+    , _apPending       = ordMovements
+    , _apMovementDelay = 0
     }
+  mdwMovements <- newTQueue
   midway <- newObject $ AirportState
-    { _apCode     = "MDW"
-    , _apName     = "Chicago Midway International Airport"
-    , _apCapacity = 700
-    , _apAircraft = S.empty
+    { _apCode          = "MDW"
+    , _apName          = "Chicago Midway International Airport"
+    , _apCapacity      = 700
+    , _apAircraft      = S.empty
+    , _apPending       = mdwMovements
+    , _apMovementDelay = 0
     }
   chicago <- newObject $ CityState
     { _cName       = "Chicago"
     , _cPopulation = 9700000
     , _cAirports   = S.fromList [oHare, midway]
     }
+  mspMovements <- newTQueue
   msp <- newObject $ AirportState
-    { _apCode     = "MSP"
-    , _apName     = "Minneapolis–Saint Paul International Airport"
-    , _apCapacity = 1100
-    , _apAircraft = S.empty
+    { _apCode          = "MSP"
+    , _apName          = "Minneapolis–Saint Paul International Airport"
+    , _apCapacity      = 1100
+    , _apAircraft      = S.empty
+    , _apPending       = mspMovements
+    , _apMovementDelay = 0
     }
   minneapolis <- newObject $ CityState
     { _cName       = "Minneapolis–St. Paul"
@@ -46,6 +66,10 @@ demo = atomically $ do
     , _gAirports  = S.fromList [oHare, midway, msp]
     , _gAircraft  = S.empty
     , _gAirborne  = S.empty
-    , _gMoney     = 100000
+    , _gMoney     = 1000000
+    , _gModels    = S.fromList [model737800]
+    , _gFlights   = M.empty
+    , _gWeek      = 0
+    , _gTime      = TimeOfWeek 0
     }
 
--- src/Types.hs
+++ src/Types.hs
@@ -2,7 +2,9 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Types where
 
+import Control.Concurrent.STM
 import Control.Lens.TH
+import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.Text as T
 import H.Prelude
@@ -16,9 +18,7 @@ data Model =
   , _mSpeed :: Integer
   , _mRange :: Integer
   , _mCost  :: Integer
-  , _mFuel  :: Integer
   , _mSeats :: Integer
-  , _mPrice :: Integer
   } deriving (Eq, Ord, Show)
 
 makeLenses ''Model
@@ -27,30 +27,61 @@ data AircraftState =
   AircraftState
   { _acId    :: T.Text
   , _acModel :: Model
-  } deriving (Eq, Ord, Show)
+  } deriving (Show)
 
 makeLenses ''AircraftState
 
+newtype Minutes = Minutes Int
+  deriving (Eq, Ord, Num, Real, Integral, Enum, Bounded, Show)
+
 type Aircraft = Object AircraftState
+
+data Movement = Landing AircraftFlight | Takeoff AircraftFlight
+  deriving (Eq, Ord)
 
 data AirportState =
   AirportState
-  { _apCode     :: T.Text
-  , _apName     :: T.Text
-  , _apCapacity :: Integer
-  , _apAircraft :: S.Set Aircraft
+  { _apCode          :: T.Text
+  , _apName          :: T.Text
+  , _apCapacity      :: Integer
+  , _apAircraft      :: S.Set Aircraft
+  , _apPending       :: TQueue Movement
+  , _apMovementDelay :: Minutes
+  }
+
+type Airport = Object AirportState
+
+newtype TimeOfWeek = TimeOfWeek { offset :: Minutes }
+  deriving (Eq, Ord, Show)
+
+data Flight =
+  Flight
+  { _fOrigin      :: Airport
+  , _fDestination :: Airport
+  , _fModel       :: Model
+  , _fTime        :: TimeOfWeek
   } deriving (Eq, Ord)
+
+data AircraftFlightState =
+  AircraftFlightState
+  { _afsAircraft :: Aircraft
+  , _afsFlight   :: Flight
+  } deriving (Eq, Ord)
+
+type AircraftFlight = Object AircraftFlightState
 
 makeLenses ''AirportState
 
-type Airport = Object AirportState
+makeLenses ''Flight
+
+makeLenses ''AircraftFlightState
 
 data CityState =
   CityState
   { _cName       :: T.Text
   , _cPopulation :: Integer
   , _cAirports   :: S.Set Airport
-  } deriving (Eq, Ord)
+  }
 
 makeLenses ''CityState
 
@@ -62,9 +93,13 @@ data GameState =
   , _gAirports  :: S.Set Airport
   , _gDistances :: CM.CrossMap Airport Integer
   , _gAircraft  :: S.Set Aircraft
-  , _gAirborne  :: S.Set Aircraft
+  , _gAirborne  :: S.Set AircraftFlight
   , _gMoney     :: Integer
-  } deriving (Eq, Ord)
+  , _gModels    :: S.Set Model
+  , _gFlights   :: M.Map TimeOfWeek (S.Set Flight)
+  , _gWeek      :: Integer
+  , _gTime      :: TimeOfWeek
+  }
 
 makeLenses ''GameState
 
