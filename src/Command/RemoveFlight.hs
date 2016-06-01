42da489f8498a14803cb01c2b284c418358054b9
Karl Voelker <karl@karlv.net>
2016-05-31 22:14:06 -0700

    Implemented RemoveFlight

--- src/Command/RemoveFlight.hs
+++ src/Command/RemoveFlight.hs
@@ -1,9 +1,11 @@
 
 module Command.RemoveFlight where
 
+import qualified Data.Map as M
 import H.Prelude
 
 import Command
+import Object
 import Types
 
 data RemoveFlight = RemoveFlight FlightNumber
@@ -11,5 +13,5 @@ data RemoveFlight = RemoveFlight FlightNumber
 
 instance Command RemoveFlight where
   type Response RemoveFlight = ()
-  runCommand _ = todo
+  runCommand (RemoveFlight flightNumber) = atomically $ getGame >>= overObject gFlights (M.delete flightNumber)
 
