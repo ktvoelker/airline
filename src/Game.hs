58c024026c462213a281f4c864c8f7a6458f207e
Karl Voelker <karl@karlv.net>
2016-07-10 18:56:50 -0700

    Broke down the top level of simulation a bit more

--- src/Game.hs
+++ src/Game.hs
@@ -3,6 +3,7 @@ module Game where
 
 import Control.Lens
 import qualified Data.Map as M
+import qualified Data.Set as S
 import H.IO
 import H.Prelude
 
@@ -11,14 +12,33 @@ import Parallel
 import Types
 
 gameSim :: Game -> IO ()
-gameSim g = parallelize gameSplitter (gameProcessor g) gameJoiner g
+gameSim g = do
+  parallelize splitByAirport (enqueueTakeoffs g) nullJoiner g
+  (takeoffAirports, landingAirports) <- partitionByNextMovement g
+  parallelize (const $ pure takeoffAirports) (executeTakeoff g) nullJoiner g
+  parallelize splitByAircraftFlight (flyAircraft g) nullJoiner g
+  parallelize (const $ pure landingAirports) (executeLanding g) nullJoiner g
 
-gameSplitter :: Game -> IO [Airport]
-gameSplitter g = view (gAirports . to M.elems) <$> readObject g
+splitByAirport :: Game -> IO [Airport]
+splitByAirport g = view (gAirports . to M.elems) <$> readObject g
 
-gameJoiner :: Game -> [()] -> IO ()
-gameJoiner = const . const $ pure ()
+splitByAircraftFlight :: Game -> IO [AircraftFlight]
+splitByAircraftFlight g = view (gAirborne . to S.toList) <$> readObject g
 
-gameProcessor :: Game -> Airport -> IO ()
-gameProcessor = todo
+nullJoiner :: Game -> [()] -> IO ()
+nullJoiner = const . const $ pure ()
 
+enqueueTakeoffs :: Game -> Airport -> IO ()
+enqueueTakeoffs = todo
+
+partitionByNextMovement :: Game -> IO ([Airport], [Airport])
+partitionByNextMovement = todo
+
+executeTakeoff :: Game -> Airport -> IO ()
+executeTakeoff = todo
+
+flyAircraft :: Game -> AircraftFlight -> IO ()
+flyAircraft = todo
+
+executeLanding :: Game -> Airport -> IO ()
+executeLanding = todo
